eb904c22a0aefa87c7380280ac9b9caf6d40d091
nick2918 <35611616+nick2918@users.noreply.github.com>
2023-07-13 22:46:05 +0000

    primer parte fetch list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	const [tarea, setTarea] = useState("")
+
+	const [lista, setLista] = useState([]) 
+
+	const crearUsuario = async () => {
+		const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/nicolaser", {
+			method: "POST", 
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json",
+			}
+
+		})
+		const data = await response.json()
+		console.log(data)
+	}
+	
+	useEffect(()=>{
+		//crearUsuario()
+		obtenerListado()
+	},[])
+
+	useEffect(()=>{
+		actualizarTareas()
+	},[lista])
+
+	const obtenerListado = async () => {
+		try {
+			const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/nicolaser") 
+			const data = await response.json()
+			setLista(data) 
+		} catch(error){
+			console.log(error)
+		}
+	}
+
+	function nuevaTarea(e) {
+		e.preventDefault()
+		setLista([...lista,{"label":tarea, "done":false}])
+		setTarea("")
+	}
+
+	const actualizarTareas = async () => {
+		const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/nicolaser", {
+			method: "PUT", 
+			body: JSON.stringify(lista),
+			headers: {
+				"Content-Type": "application/json",
+			}
+
+		})
+		const data = await response.json()
+		console.log(data)
+	}
+
+	function eliminar(id) {
+		let nuevoArray = []
+		nuevoArray = lista.filter((item, index)=>{
+			if(index!== id){
+				return item
+			}
+		})
+		setLista(nuevoArray)
+
+	}
+		
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center">
+			<input
+			type="text" 
+			className="form-control"
+			value={tarea}
+			onChange={(e)=> setTarea(e.target.value)} />
+			<button className="btn btn-outline-secondary" onClick={nuevaTarea}>Agregar Tarea</button>
+			<div>
+				<ul className="list-group">
+					{lista.map((item, id)=>(
+						<li key={id} className="list-group-item">{item.label}
+						<button className="btn btn-outline-secondary float-end" onClick={()=> eliminar(id)}>
+							{/* <i className="fa-solid fa-trash"></i> */}
+							X
+						</button>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
